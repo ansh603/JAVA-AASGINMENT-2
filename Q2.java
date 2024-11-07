5256b92b8c727b5aba5e1c13cf534948b9c8c8b3
ansh603 <ghanshyambatane12362@gmail.com>
2024-11-07 18:12:27 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Q2{
+public static void main(String[] args){
+Scanner a= new Scanner(System.in);
+System.out.println("enter a no.");
+int n=a.nextInt();
+if(n>0){
+    System.out.println("positive");
+}
+else if(n<0){
+    System.out.println("negative");
+}
+
+
+}
+}
